83c69e761a0cdae71eeefb77b406bdf193c39ea5
andrxpie <andrxpie@gmail.com>
2024-12-22 09:22:45 +0200

    3.Hibernate create

--- Classworks/2.OOP/src/main/java/org/example/DatabaseWorker.java
+++ Classworks/2.OOP/src/main/java/org/example/DatabaseWorker.java
@@ -1,36 +1,52 @@
 package org.example;
 
-import javax.swing.plaf.nimbus.State;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class DatabaseWorker {
-    private String host = "ep-proud-feather-a5ayx291.us-east-2.aws.neon.tech";
-    private String url = "jdbc:postgresql://"+host+":5432/neondb";
-    private String user = "neondb_owner";
-    private String password = "r2PKbS1IupFV";
 
     Connection connection = null;
 
     public DatabaseWorker() {
         // Initialize the connection
         try {
-            connection = DriverManager.getConnection(url, user, password);
+            ConnectDatabase();
             if (connection != null) {
                 System.out.println("Connected to the database!");
 
             } else {
                 System.out.println("Failed to make connection!");
             }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             System.out.println("Connection failure.");
+        }
+    }
+
+    public void ConnectDatabase() {
+        try {
+            Path DBPath = Path.of(Objects
+                    .requireNonNull(getClass().getClassLoader().getResource("option.json")).toURI());
+            JsonNode root = new ObjectMapper().readTree(DBPath.toFile());
+
+            String host = root.get("host").asText();
+            String username = root.get("username").asText();
+            String password = root.get("password").asText();
+            String namedb = root.get("namedb").asText();
+
+            String url = "jdbc:postgresql://"+host+":5432/"+namedb;
+
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -57,4 +73,15 @@ public class DatabaseWorker {
             System.err.println("Error in reading file: " + e.getMessage());
         }
     }
+
+    /*public void createContact(String firstName, String lastName, String email, String phone) {
+        String time = String.valueOf(LocalDateTime.now());
+        String request = "INSERT INTO Contacts (FirstName, LastName, Email, PhoneNumber) VALUES ("+firstName+","+lastName+","+email+","+phone+","+time+")";
+        try {
+            PreparedStatement statement = connection.prepareStatement(request);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }*/
 }
--- Classworks/2.OOP/src/main/java/org/example/Main.java
+++ Classworks/2.OOP/src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
