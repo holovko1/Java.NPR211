424e3c3ae0be500f909f2e978b07fbad6870d5da
andrxpie <andrxpie@gmail.com>
2025-01-19 13:18:22 +0200

    Image show

---
+++ Classworks/4.SpringBoot/MyAPI/README.md
@@ -0,0 +1,7 @@
+mvn clean package
+
+mvn spring-boot:run
+
+http://localhost:8081/swagger-ui/index.html
+
+java -jar target/npr211.jar
--- Classworks/4.SpringBoot/MyAPI/src/main/java/org/example/config/WebConfig.java
+++ Classworks/4.SpringBoot/MyAPI/src/main/java/org/example/config/WebConfig.java
@@ -2,6 +2,7 @@ package org.example.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,10 +10,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5175") // Add the appropriate origin of your client application
+                .allowedOrigins("http://localhost:5173") // Add the appropriate origin of your client application
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:photos/")
+                .setCachePeriod(30); // Кешування в секундах
     }
 }
